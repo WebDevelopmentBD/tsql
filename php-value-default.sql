04c92d0db5917e1cc50ec064e321491c31d028f3
Web Development BD <abbasuddin@outlook.com>
2022-10-16 12:29:21 +0600

    PHP Default Array :: from a Table
    PHP Default Array :: from a Table

--- php-value-default.sql
+++ php-value-default.sql
@@ -4,6 +4,9 @@ SET SESSION group_concat_max_len = 65535;
 SET @colwidth = (SELECT MAX(LENGTH(`COLUMN_NAME`))+4 FROM information_schema.`COLUMNS`
 WHERE `TABLE_SCHEMA`=DATABASE() AND `TABLE_NAME`=REPLACE(@tbl_name, '`',''));
 
+SET @_pklist = (SELECT IFNULL(GROUP_CONCAT(DISTINCT CONCAT('`', `COLUMN_NAME`, '` = \'{$_GET[\'', `COLUMN_NAME`,'\']}\'') SEPARATOR ' AND '),'1') AS `PK`
+FROM `INFORMATION_SCHEMA`.`KEY_COLUMN_USAGE` WHERE `TABLE_SCHEMA` = DATABASE() AND `TABLE_NAME` = REPLACE(@tbl_name, '`',''));
+
 START TRANSACTION;
 CREATE TEMPORARY TABLE IF NOT EXISTS php_schema AS (
   SELECT `COLUMN_NAME` AS ff, `COLUMN_DEFAULT` AS `fv`, CONCAT(`COLUMN_COMMENT`,' ',`EXTRA`) AS `remarks`, UCASE(`DATA_TYPE`) AS `datatype`, `IS_NULLABLE` AS `allow_null`
@@ -33,7 +36,10 @@ CONCAT(
   '//Default values of "',@tbl_name,'"\nlet record = {\n\t',
   GROUP_CONCAT(CONCAT('\'', `ff`, '\'',LPAD(': ', @colwidth-LENGTH(`ff`), ' '), LCASE(`fv`), '\t  //@', `datatype`, ': ', `remarks`) SEPARATOR '\n\t, '),
   '\n};\n'
-) AS `JS_VALUE`
+) AS `JS_VALUE`,
+CONCAT(
+  '//Existing row\n$record = $link->query("', 'SELECT * FROM ', DATABASE(), '.', @tbl_name, ' WHERE ', @_pklist, '")->fetch_object();'
+) AS `MySQLi`
 FROM `php_schema`;
 
 SELECT * FROM php_schema;
